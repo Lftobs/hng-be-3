80ace269fe5abbe2a397d70c9035054d5fc31d4a
lftobs <tobbytobs1@gmail.com>
2025-02-11 04:05:30 +0100

    chore: test cd 01

--- api/routes/books.py
+++ api/routes/books.py
@@ -60,6 +60,19 @@ async def get_books_by_id(book_id: int) -> OrderedDict[int, Book]:
     return JSONResponse(
         status_code=status.HTTP_200_OK, content=book.model_dump()
     )
+    
+@router.get(
+    "rrrr/{book_id}", response_model=OrderedDict[int, Book], status_code=status.HTTP_200_OK
+)
+async def get_books_by_id(book_id: int) -> OrderedDict[int, Book]:
+    """ endpoint to get a book by id """
+    book = db.get_book(book_id)
+    if book is None:
+        return JSONResponse(status_code=status.HTTP_404_NOT_FOUND, content={"detail": "Book not found"})
+    
+    return JSONResponse(
+        status_code=status.HTTP_200_OK, content=book.model_dump()
+    )
 
 
 @router.put("/{book_id}", response_model=Book, status_code=status.HTTP_200_OK)
